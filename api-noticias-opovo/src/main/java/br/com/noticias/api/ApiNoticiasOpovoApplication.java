be6cd860d2dc06dabcdfdc50dcd704d9548b6c4f
Rafael Silv@ <rafaelnasc.silv@gmail.com>
2019-12-06 17:43:29 -0300

    criando projeto api de notícias opovo com spring-boot

---
+++ api-noticias-opovo/src/main/java/br/com/noticias/api/ApiNoticiasOpovoApplication.java
@@ -0,0 +1,13 @@
+package br.com.noticias.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiNoticiasOpovoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiNoticiasOpovoApplication.class, args);
+	}
+
+}
---
+++ api-noticias-opovo/src/test/java/br/com/noticias/api/ApiNoticiasOpovoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.noticias.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiNoticiasOpovoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
